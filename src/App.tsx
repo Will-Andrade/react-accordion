0f5f6bc40694bf2d6075196485c6ad15ade2db3a
Will-Andrade <willian.santos.andra@outlook.com>
2023-02-13 00:00:55 -0400

    feat: added the accordion's structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,40 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (<>
-    <h1>Hello World</h1>
-  </>);
+  return (<main>
+    <h1>React Accordion</h1>
+
+    <section>
+      <h2>Frequently Asked Questions</h2>
+      <article>
+        <div>
+          <h3>Is this a good product?</h3>
+          <button type='button'>+</button>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sed vel quasi! Esse, sed debitis voluptatum perferendis repellendus inventore quasi, molestias corrupti voluptas laudantium, labore alias beatae fugit natus ipsam.
+        </p>
+      </article>
+      <article>
+        <div>
+          <h3>How much does it cost?</h3>
+          <button type='button'>+</button>
+        </div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque suscipit dolorem minima culpa nemo dolor quod iusto quae dolorum cupiditate molestias nobis aliquid facilis amet similique, maiores sint saepe consequatur.
+        </p>
+      </article>
+      <article>
+        <div>
+          <h3>When can I get it?</h3>
+          <button type='button'>+</button>
+        </div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque suscipit dolorem minima culpa nemo dolor quod iusto quae dolorum cupiditate molestias nobis aliquid facilis amet similique, maiores sint saepe consequatur.
+        </p>
+      </article>
+    </section>
+  </main>);
 }
 
 export default App;
